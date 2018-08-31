54846b6618e1667e7c2de34f4162cd59981dba67
scarlettperry <si.perry15@gmail.com>
2018-08-31 14:45:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function boardMembers(name, homestate, training) {
+function BoardMember(name, homeState, training) {
   this.name = name
-  this.homestate = homestate
+  this.homeState = homeState
   this.training = training
   this.veto = function() {
-    "No, I must disagree"
+    return "No, I must disagree"
     //do i use return/console.log()??
   }
   this.approve = function() {
-    "You can do that!"
+    return "You can do that!"
   }
   this.doCharity = function() {
-    "I like to help people."
+    return "I like to help people."
   }
   this.releasePressStatement = function() {
-    "You will see great things from Scuber."
+    return "You will see great things from Scuber."
   }
   this.sayHi = function() {
-    `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     //have to do ${this.method}, not just ${method}
   }
 }
